394fff5798b5a07036791253d04dd9d8dd49a56b
jpoiatti <j.poiatti@gmail.com>
2019-04-21 17:54:43 -0300

    Add service worker and basic assets caching

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -153,6 +153,9 @@ class DBHelper {
     return (`/img/${restaurant.photograph}`);
   }
 
+  /**
+   * Restaurant responsive images URLs and view widths.
+   */
   static responsiveImageUrlForRestaurant(restaurant) {
     let imageBaseName = restaurant.photograph.split('.')[0];
     let srcSet = `/img/${imageBaseName}/${imageBaseName}_400.jpg 400w, /img/${imageBaseName}/${imageBaseName}_600.jpg 600w, /img/${imageBaseName}/${imageBaseName}_800.jpg 800w`;
